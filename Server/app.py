7821f54e550d83b193471b6c24c1e35ff19c49ee
MuadDib1 <yushuan_shieh@yahoo.com>
2020-07-01 15:00:30 +0900

    added dynamic port number

--- Server/app.py
+++ Server/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,4 @@ def hello():
     return 'Hello World!\n'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get('PORT', 5000)))
