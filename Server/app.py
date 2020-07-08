73010e8836f4e262430d0b450d7f5af1e92cf744
MuadDib1 <yushuan_shieh@yahoo.com>
2020-07-08 21:24:54 +0900

    yolo.h5を外した

--- Server/app.py
+++ Server/app.py
@@ -1,11 +1,41 @@
-import os
-from flask import Flask
+import os, human_detect
+from flask import Flask, flash, request, redirect, \
+    url_for, render_template
+
+UPLOAD_FOLDER = '/code/image_in/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def hello():
     return 'Hello World!\n'
+
+@app.route('/post-picture', methods=['GET', 'POST'])
+def post_picture():
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'pic1' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['pic1']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            human_detect.analyse_upl_img(file.filename)    
+            return '成功\n'
+
+    return render_template("client.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=int(os.environ.get('PORT', 5000)))
---
+++ Server/human_detect.py
@@ -0,0 +1,31 @@
+from yolo import YOLO, detect_video
+from PIL import Image
+import numpy as np
+
+def detect_img(yolo):
+    while True:
+        img = input('Input image filename:')
+        try:
+            image = Image.open(f"./image_in/{img}")
+        except:
+            print('Open Error! Try again!')
+            continue
+        else:
+            r_image, human_pos_list = yolo.detect_human(image)
+            print(human_pos_list)
+            r_image.save(f"./image_out/{img}")
+            r_image.show()
+    yolo.close_session()
+
+def analyse_upl_img(filename):
+    yolo = YOLO()
+    image = Image.open(f"./image_in/{filename}")
+    r_image, human_pos_list = yolo.detect_human(image)
+    print(human_pos_list)
+    r_image.save(f"./image_out/{filename}")
+    yolo.close_session()
+
+
+
+if __name__ == '__main__':
+    detect_img(YOLO())
