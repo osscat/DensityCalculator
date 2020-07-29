791080d7f23305971ef34f32a7a3faf36b60cd45
MuadDib1 <yushuan_shieh@yahoo.com>
2020-07-29 16:53:23 +0900

    計算式が完成

--- Server/human_detect.py
+++ Server/human_detect.py
@@ -26,6 +26,15 @@ def analyse_upl_img(filename):
     yolo.close_session()
 
 
+def distance_2_ppl(first_person, second_person, pic_length_in_pixel, cmos_length, 
+    focus_length, camera_mov_delta):
+    pixel_delta
+    distance_from_camera
+    distance_from_centre_pixel
+    distance_from_centre_mm
+    angle_from_centre
+    return distance_btw_people
+
 
 if __name__ == '__main__':
     detect_img(YOLO())
