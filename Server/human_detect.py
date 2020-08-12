1909fe628c8ceb6aa9477cd456db751352663e3b
Naotaka Nakanishi <nakanishi-naotaka@osscat.net>
2020-08-12 15:17:11 +0900

    不要なコードを削除

--- Server/human_detect.py
+++ Server/human_detect.py
@@ -25,15 +25,5 @@ def analyse_upl_img(yolo, filename):
     return human_pos_list
 
 
-def distance_2_ppl(first_person, second_person, pic_length_in_pixel, cmos_length, 
-    focus_length, camera_mov_delta):
-    pixel_delta
-    distance_from_camera
-    distance_from_centre_pixel
-    distance_from_centre_mm
-    angle_from_centre
-    return distance_btw_people
-
-
 if __name__ == '__main__':
     detect_img(YOLO())
