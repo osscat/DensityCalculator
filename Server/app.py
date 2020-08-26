5309e892bf0e0f0db59bb3c8baeb5e04e2c3d0eb
Naotaka Nakanishi <nakanishi-naotaka@osscat.net>
2020-08-26 15:26:13 +0900

    密判定のWeb APIを追加

--- Server/app.py
+++ Server/app.py
@@ -32,38 +32,51 @@ def save_picture():
 
     return {'message': '保存しました'}
 
+@app.route('/api/detect_mitsu', methods=['POST'])
+def api_detect_mitsu():
+    try:
+        result, distance = do_detect_mitsu()
+        return {
+            'mitsu': result,
+            'distance': distance
+        }
+    except RuntimeError as err:
+        return {'message': str(err)}
+
 @app.route('/detect_mitsu', methods=['GET', 'POST'])
 def detect_mitsu():
     if request.method == 'POST':
-        camera_mov_delta = get_float('move')
-        if camera_mov_delta == 0:
-            flash('カメラの移動距離を入力してください')
+        try:
+            result, distance = do_detect_mitsu()
+            return render_template("result.html", mitsu=result, distance='{:.2f}'.format(distance))
+        except RuntimeError as err:
+            flash(str(err))
             return redirect(request.url)
-
-        cmos_length = get_float('cmos')
-        if cmos_length == 0:
-            flash('撮像素子サイズを入力してください')
-            return redirect(request.url)
-
-        if not valid_file('pic1') or not valid_file('pic2'):
-            flash('写真を2つアップロードしてください')
-            return redirect(request.url)
-
-        pos_list1 = get_pos_list(request.files['pic1'])
-        pos_list2 = get_pos_list(request.files['pic2'])
-        if len(pos_list1) <= 1 or len(pos_list2) <= 1:
-            flash('2人以上写っている写真をアップロードしてください')
-            return redirect(request.url)
-
-        pic_length_in_pixel, focus_length = get_width_and_focus_length(request.files['pic1'])
-        if focus_length is None:
-            flash('メタデータに焦点距離を含む写真をアップロードしてください')
-            return redirect(request.url)
-
-        result, distance = crowded_people(pos_list1, pos_list2, pic_length_in_pixel, cmos_length, focus_length, camera_mov_delta)
-        return render_template("result.html", mitsu=result, distance='{:.2f}'.format(distance))
 
     return render_template("detect_mitsu.html")
+
+def do_detect_mitsu():
+    camera_mov_delta = get_float('move')
+    if camera_mov_delta == 0:
+        raise RuntimeError('カメラの移動距離を入力してください')
+
+    cmos_length = get_float('cmos')
+    if cmos_length == 0:
+        raise RuntimeError('撮像素子サイズを入力してください')
+
+    if not valid_file('pic1') or not valid_file('pic2'):
+        raise RuntimeError('写真を2つアップロードしてください')
+
+    pos_list1 = get_pos_list(request.files['pic1'])
+    pos_list2 = get_pos_list(request.files['pic2'])
+    if len(pos_list1) <= 1 or len(pos_list2) <= 1:
+        raise RuntimeError('2人以上写っている写真をアップロードしてください')
+
+    pic_length_in_pixel, focus_length = get_width_and_focus_length(request.files['pic1'])
+    if focus_length is None:
+        raise RuntimeError('メタデータに焦点距離を含む写真をアップロードしてください')
+
+    return crowded_people(pos_list1, pos_list2, pic_length_in_pixel, cmos_length, focus_length, camera_mov_delta)
 
 def get_float(name):
     if name not in request.form:
